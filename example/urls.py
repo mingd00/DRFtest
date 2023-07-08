0050816df76f6844a989168d7e25cc2e5dd81df7
mingd00 <rkdalswl403@naver.com>
2023-07-08 19:51:22 +0900

    [feat] api

---
+++ example/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from .views import helloAPI, bookAPI, booksAPI, BookAPI, BooksAPI
+
+urlpatterns = [
+    path("hello/", helloAPI),
+    path("fbv/books/", booksAPI),
+    path("fbv/book/<int:bid>/", bookAPI),
+    path("cbv/books/", BooksAPI.as_view()),
+    path("cbv/book/<int:bid>/", BookAPI.as_view()),
+]
